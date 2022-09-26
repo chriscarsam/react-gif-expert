3d2b334efd14d5a6e4295dcec9d3ddb81163edb8
chriscar.sam <chriscar.sam@gmail.com>
2022-09-26 17:35:36 -0500

    Comunicación entre componentes

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react"
 
-export const AddCategory = () => {
+export const AddCategory = ({ setCategories }) => {
 
-    const [inputValue, setinputValue] = useState('One Punch');
+    const [inputValue, setinputValue] = useState('');
 
     const onInputChange = ({ target }) => {        
         setinputValue( target.value );
@@ -10,11 +10,15 @@ export const AddCategory = () => {
 
     const onSubmit = ( event ) => {
         event.preventDefault();
-        console.log(inputValue);
+        if( inputValue.trim().length <= 1) return;
+
+        setCategories( categories => [ inputValue, ...categories ]);  
+        setinputValue('');     
+
     }
 
   return (
-    <form onSubmit={ (event)=> onSubmit(event) }>
+    <form onSubmit={ onSubmit }>
         <input 
         type="text" 
         placeholder="Buscar gifs" 
