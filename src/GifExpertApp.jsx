f02557f3bc81f8bc524195e03f64cfac58dfd591
chriscar.sam <chriscar.sam@gmail.com>
2022-09-27 13:42:15 -0500

    Validar que sea únicos los nombres

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -6,6 +6,8 @@ export const GifExpertApp = () => {
   const [categories, setCategories] = useState([ 'One Punch', 'Dr. Stone', 'Naruto' ]);
 
   const onAddCategory = ( newCategory ) => {
+
+    if( categories.includes(newCategory)) return;
         
     setCategories([ newCategory, ...categories ]);
     // setCategories( cat => [ ...cat, 'Valorant']);
