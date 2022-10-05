77db9502258a6f406806204147f7698810862d90
chriscar.sam <chriscar.sam@gmail.com>
2022-10-05 17:00:18 -0500

    Archivos de barril

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
+import { AddCategory, GifGrid } from "./components";
 
 export const GifExpertApp = () => {
 
@@ -11,16 +10,14 @@ export const GifExpertApp = () => {
     if( categories.includes(newCategory)) return;
         
     setCategories([ newCategory, ...categories ]);
-    // setCategories( cat => [ ...cat, 'Valorant']);
-
+    
   }
 
   return (
     <>    
     <h1>GifExpertApp</h1>   
     <AddCategory  
-      // setCategories={ setCategories }
-      onNewCategory={ value => onAddCategory(value) }
+            onNewCategory={ value => onAddCategory(value) }
      />    
    
       {
