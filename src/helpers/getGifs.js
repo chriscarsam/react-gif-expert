9e1c29ddc530283dd722b433f2b30f8e7cd7d571
chriscar.sam <chriscar.sam@gmail.com>
2022-10-05 16:23:45 -0500

    className - Clases de css

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
  export const getGifs = async( category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=6AChXiqz6XNOYyvSE9AnKI4JoXvdwHFh&q=${ category }&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=6AChXiqz6XNOYyvSE9AnKI4JoXvdwHFh&q=${ category }&limit=10`;
     const resp = await fetch(url);
     const { data } = await resp.json();
   
